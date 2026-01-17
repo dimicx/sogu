64e989e1d11df008e58939c61c36a73442e28ec8
dima <dumitrucepoi03@gmail.com>
2026-01-17 00:22:27 +0000

    update layout to include Fetta logo as a link to the homepage

--- website/app/docs/layout.tsx
+++ website/app/docs/layout.tsx
@@ -1,13 +1,21 @@
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { source } from "@/app/source";
+import { FettaLogo } from "../components/fetta-logo";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout
       tree={source.pageTree}
       nav={{
-        title: <span className="font-bold">fetta</span>,
+        title: (
+          <Link href="/" aria-label="Fetta homepage" title="Fetta homepage">
+            <span aria-hidden="true" className="h-6 block">
+              <FettaLogo />
+            </span>
+          </Link>
+        ),
       }}
     >
       {children}
